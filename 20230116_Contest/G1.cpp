7d50b3b2280aa95d50d74082e665a305c9b356de
SB-YounG <SB-YounG@outlook.com>
2023-01-17 19:39:30 +0800

    G

--- 20230116_Contest/G1.cpp
+++ 20230116_Contest/G1.cpp
@@ -1,13 +1,13 @@
 /*-*- gcc 10.3.0 -*-*/
 /*-*- coding:utf-8 -*-*/
 /***************************************************************************************************
-* File: \G.cpp                                                                                     *
+* File: \G1.cpp                                                                                    *
 * Project: 20230116_Contest                                                                        *
-* Created Date: Monday Jan 16th 2023, 4:58:13 pm                                                   *
+* Created Date: Tuesday Jan 17th 2023, 7:11:14 pm                                                  *
 * Author: Wenren Muyan                                                                             *
 * Comments:                                                                                        *
 * --------------------------------------------------------------------------------                 *
-* Last Modified: 17/01/2023 07:22:59                                                               *
+* Last Modified: 17/01/2023 07:36:44                                                               *
 * Modified By: Wenren Muyan                                                                        *
 * --------------------------------------------------------------------------------                 *
 * Copyright (c) 2023 - future Wenren Muyan                                                         *
@@ -20,6 +20,8 @@
 
 #include <iostream>
 #include <cmath>
+#include <algorithm>
+#include <set>
 
 using namespace std;
 
@@ -29,10 +31,10 @@ long long f(long long x){
     return (long long)round(10 * sqrt(x));
 }
 
-// ERROR: tle
 int main(){
     int n, m, op, l, r, k, i, j, q, s = 0;
     long long * a, sum = 0, * res, * search;
+    set<int> st;
 
     // for(i = 0; i < INF; i++){
 
@@ -41,40 +43,44 @@ int main(){
     cin >> n >> m;
     a = new long long[n];
     res = new long long[m];
-    search = new long long[n];
     for(i = 0; i < n; i++){
         cin >> a[i];
-        search[i] = f(a[i]);
+        if(a[i] != f(a[i])) st.insert(i);
         sum += a[i];
     }
+    
+    st.insert(n);
 
-    for(j = 0; j < m; j++){
+    for(i = 0; i < m; i++){
         cin >> op;
         if(op == 1){
             cin >> l >> r >> k;
-            for(i = l; i <= r; i++){
-                if(a[i - 1] != search[i - 1]){
-                    sum -= a[i - 1];
-                    for(q = 0; q < min(k, 20); q++){
-                        a[i - 1] = search[i - 1];
-                        search[i - 1] = (long long)round(10 * sqrt(a[i - 1]));
-                        // cout << a[i - 1] << " ";
-                        if(a[i - 1] == search[i - 1]) break;
+            j = l - 1;
+            while(1){
+                j = *st.lower_bound(j);
+                // cout << j << endl;
+                if(j > r - 1) break;
+                else{
+                    for(q = 0; q < min(20, k); q++){
+                        sum -= a[j];
+                        a[j] = f(a[j]);
+                        sum += a[j];
+                        // cout << a[j] << " " << f(a[j]) << endl;
                     }
-                    sum += a[i - 1];
+                    if(a[j] == f(a[j])) st.erase(j);
+                    j++;
                 }
             }
         }
         else if(op == 2){
-            // cout << sum << endl;
             res[s++] = sum;
         }
     }
 
-    if(s)
-        for(i = 0; i < s; i++) cout << res[i] << endl;
-
-    // cout << f(0) << endl;
+    for(i = 0; i < s; i++){
+        cout << res[i] << endl;
+    }
 
     return 0;
+
 }

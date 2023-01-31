2d61a5d17a138bfcab0d524db01318c123d41055
SB-YounG <SB-YounG@outlook.com>
2023-01-31 14:09:16 +0800

    some update

--- Algorithm/PrimeFilter/primeFilter.cpp
+++ Algorithm/PrimeFilter/primeFilter.cpp
@@ -7,7 +7,7 @@
 * Author: Wenren Muyan                                                                             *
 * Comments:                                                                                        *
 * --------------------------------------------------------------------------------                 *
-* Last Modified: 31/01/2023 01:58:31                                                               *
+* Last Modified: 31/01/2023 02:08:54                                                               *
 * Modified By: Wenren Muyan                                                                        *
 * --------------------------------------------------------------------------------                 *
 * Copyright (c) 2023 - future Wenren Muyan                                                         *
@@ -18,3 +18,55 @@
 ***************************************************************************************************/
 
 
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+using namespace std;
+
+#define MAXSIZE 30000000
+
+bool isPrime[MAXSIZE];
+int prime[MAXSIZE], primeNum = 0;                // a array to storage prime numbers 
+
+// filtrate if the number is prime under maximum max(inlcuding max), 
+// and the prime numbers' quantity is primeNum, 
+// all of them are storaged in prime array
+void eratosthenesFilter(int max){  
+    memset(isPrime, true, sizeof(isPrime));              
+    isPrime[0] = isPrime[1] = false;
+    int i, j;
+    for(i = 2; i <= max; i++){
+        if(isPrime[i]){
+            prime[primeNum++] = i;
+            for(j = i * i; j <= max; j += i)
+                isPrime[j] = false;
+        }
+    }
+}
+
+void eulerFilter(int max){
+    memset(isPrime, true, sizeof(isPrime));
+    isPrime[0] = isPrime[1] = false;
+    int i, j;
+    for(i = 2; i <= max; i++){
+        if(isPrime[i])
+            prime[primeNum++] = i;
+        for(j = 0; j < primeNum && i * prime[j] <= max; j++){
+            isPrime[i * prime[j]] = false;
+            if(!(i % prime[j])) break;
+        }
+    }
+}
+
+bool isPrimePlain(int n){
+    int i;
+    double s = sqrt(double(n));
+    if(n == 1) return false;
+    if(n == 2) return true;
+
+    for(i = 2; i <= s; i++)
+        if(!(n % i)) return false;
+    
+    return true;
+}
